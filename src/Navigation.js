e8734fecba23e2d6510e4af675cafae4e370932c
Franklin <antonyfranklin1710@gmail.com>
2026-09-23 12:20:43 +0530

    Create Navigation.js

---
+++ src/Navigation.js
@@ -0,0 +1,15 @@
+import  React from "react";
+import {Link} from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+function Navigation(){
+    return(
+    <nav>
+       <Link to="/">Home</Link>
+       <Link to="/about">About</Link>
+       <Link to="/contact">Contact</Link>
+    </nav>
+    );
+}
+export default Navigation;
